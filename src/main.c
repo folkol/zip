71d7599bfcd522fd1bbadb4fe75d497c582ed49c
Mattias Johansson <mattias.johansson@atex.com>
2013-04-25 15:19:45 +0200

    Now parsing end of directory and printing the results

--- src/main.c
+++ src/main.c
@@ -31,24 +31,27 @@
 
 FILE *file;
 
+const long DIR_SIGNATURE = 0x06054b50;
+
 struct {
-  long  signature;                  // end of central dir signature    4 bytes  (0x06054b50)
-  short disk_no;                    // number of this disk             2 bytes
-  short dir_disk_no;                // number of the disk with the start of the central directory  2 bytes
-  short dir_entries_this_disk;      // total number of entries in the central directory on this disk  2 bytes
-  short dir_entries;                // total number of entries in the central directory           2 bytes
-  long  dir_size;                   // size of the central directory   4 bytes
-  long  dir_offset;                 // offset of start of central directory with respect to the starting disk number        4 bytes
-  short comment_length;             // .ZIP file comment length        2 bytes
-  char* comment;                    // .ZIP file comment       (variable size)
-} end_of_directory;
+  short disk_no;                     // number of this disk (2 bytes)
+  short dir_disk_no;                 // number of the disk with the start of the central directory (2 bytes)
+  short dir_entries_this_disk;       // total number of entries in the central directory on this disk (2 bytes)
+  short dir_entries;                 // total number of entries in the central directory (2 bytes)
+  long  dir_size;                    // size of the central directory (4 bytes)
+  long  dir_offset;                  // offset of start of central directory with respect to the starting disk number (4 bytes)
+  short comment_length;              // .ZIP file comment length (2 bytes)
+  char* comment;                     // .ZIP file comment (variable size)
+} end_of_dir;
+
+char* archive_comment;
 
 void print_usage() {
   fprintf(stderr, "usage: fzip filename\n");
   exit(EXIT_FAILURE);
 }
 
-void read_end_of_directory() {
+void read_end_of_dir() {
   long offset = -4;
   long signature;
   while(1) {
@@ -57,14 +60,35 @@ void read_end_of_directory() {
       exit(EXIT_FAILURE);
     }
     fread(&signature, 4, 1, file);
-    if(signature == 0x06054b50) {
-      printf("Found end of directory at %li!", ftell(file));
-      exit(EXIT_SUCCESS);
+    if(signature == DIR_SIGNATURE) {
+      printf("Found end of directory at %li!\n", ftell(file));
+
+      fread(&end_of_dir.disk_no, 2, 1, file);
+      fread(&end_of_dir.dir_disk_no, 2, 1, file);
+      fread(&end_of_dir.dir_entries_this_disk, 2, 1, file);
+      fread(&end_of_dir.dir_entries, 2, 1, file);
+      fread(&end_of_dir.dir_size, 4, 1, file);
+      fread(&end_of_dir.dir_offset, 4, 1, file);
+      fread(&end_of_dir.comment_length, 2, 1, file);
+      archive_comment = (char*) malloc(end_of_dir.comment_length + 1);
+      fread(archive_comment, end_of_dir.comment_length, 1, file);
+      archive_comment[end_of_dir.comment_length] = '\0';
+      end_of_dir.comment = archive_comment;
+
+      printf("End of directory structure:\n");
+      printf("disk_no: %hd\n", end_of_dir.disk_no);
+      printf("dir_disk_no: %hd\n", end_of_dir.dir_disk_no);
+      printf("dir_entries_this_disk: %hd\n", end_of_dir.dir_entries_this_disk);
+      printf("dir_entries: %hd\n", end_of_dir.dir_entries);
+      printf("dir_size: %ld\n", end_of_dir.dir_size);
+      printf("dir_offset: %ld\n", end_of_dir.dir_offset);
+      printf("comment_length: %hd\n", end_of_dir.comment_length);
+      printf("comment: \"%s\".\n", end_of_dir.comment);
+
+      break;
     }
     offset--;
   }
-  fprintf(stderr, "Could not find end of directory...");
-  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char** argv) {
@@ -79,7 +103,7 @@ int main(int argc, char** argv) {
     return;
   }
 
-  read_end_of_directory();
+  read_end_of_dir();
 
   fclose(file);
 
