e7e724fa9001d68361f86b09685a2e8fdeb9a71c
Mattias Johansson <mattias.johansson@atex.com>
2013-04-25 12:48:34 +0200

    Added comment about file format overview

--- src/main.c
+++ src/main.c
@@ -1,3 +1,29 @@
+/*
+ 4.3.6 Overall .ZIP file format:
+
+      [local file header 1]
+      [encryption header 1]
+      [file data 1]
+      [data descriptor 1]
+      .
+      .
+      .
+      [local file header n]
+      [encryption header n]
+      [file data n]
+      [data descriptor n]
+      [archive decryption header]
+      [archive extra data record]
+      [central directory header 1]
+      .
+      .
+      .
+      [central directory header n]
+      [zip64 end of central directory record]
+      [zip64 end of central directory locator]
+      [end of central directory record]
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <zlib.h>
